f5a147f065b0187d9b065318508ae280ae98f000
LORENZO <LORENZO@PC-LORENZO>
2019-11-18 11:51:57 +0100

    Initial commit

---
+++ ProgettoOOP/src/test/java/org/univpm/projectoop/ProgettoOopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.univpm.projectoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoOopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
