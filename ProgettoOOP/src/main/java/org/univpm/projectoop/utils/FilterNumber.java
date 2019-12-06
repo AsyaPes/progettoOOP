054af9321bbd1295eaa46d622c6ff79564c59af1
LolloVagnoloo <57802876+LolloVagnoloo@users.noreply.github.com>
2019-12-06 12:18:22 +0100

    Aggiornamento classe FilterNumber

--- ProgettoOOP/src/main/java/org/univpm/projectoop/utils/FilterNumber.java
+++ ProgettoOOP/src/main/java/org/univpm/projectoop/utils/FilterNumber.java
@@ -1,69 +1,50 @@
 package org.univpm.projectoop.utils;
 
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.List;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
-import org.univpm.projectoop.dataset.Stock;
 
 public class FilterNumber {
 	
 	private double avg;
-	private double sum;
-	private double min;
-	private double max;
+	private int sum;
+	private int min;
+	private int max;
 	private double dev_std;
 	private int count;
 
+	private List<Integer> stocksData;
 	
-	 private static boolean isInteger(String field) {
-        try {
-            Integer.parseInt(field);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
-    }
- 
-	public FilterNumber (ArrayList<Stock> Stocks,String field) throws Exception
+	public FilterNumber ()
 	{
-		double[] value_array=new double [count];
-		this.count=Stocks.size();
-		if( count == 0 )
-		{
-			throw new IOException("Errore, non ci sono dati corrispondenti ai filtri");
-			
-		}
-		else
-		{
-			
-			for(int i=0;i<count;i++)	
-			{
-			    Method m=null;
-			    try {
-	                m = Stock.class.getMethod("get"+field.substring(0, 1).toUpperCase()+field.substring(1),null);
-	                Object doubleValue = m.invoke(Stocks.get(i));
-	                value_array[i] = (double) doubleValue;
-	            }catch (ClassCastException e) {
-	                e.printStackTrace();
-	                throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Il formato non è corretto"); 
-	            }
-
-			}
-		}
-
-	//set
-/*
- 	    setAvg(value_array);
-		setSum(value_array);
-		setMin(value_array);
-		setMax(value_array);
-		setDev_std(value_array);
-	*/	
+		this.avg=0;
+		this.count=0;
+		this.dev_std=0;
+		this.sum=0;	
+		this.stocksData= new ArrayList<Integer>();
 	}
+	
+ 
+	public void insertValue(Integer field)
+	{
+		this.stocksData.add(field);
+		this.setSum(field);
+		this.count++;
+		
+		if(this.count == 1) 
+		{
+			this.setMin(field);
+			this.setMax(field);
+		}
+			else 
+			{
+				this.setMin(Math.min(field,this.min));
+				this.setMax(Math.max(field,this.max));
+			}
+		
+		setAvg();
+	}
+
 
 
 	public double getAvg() {
@@ -71,106 +52,69 @@ public class FilterNumber {
 	}
 
 
-	public void setAvg(double avg) {
+	private void setAvg(double avg) {
 		this.avg = avg;
 	}
   
 	public void setAvg() {
-		setAvg(sum/count);
+		setAvg((double)sum/count);
 	}
 	
 
-	public double getSum() {
+	public int getSum() {
 		return sum;
 	}
 
-
-	public void setSum(double sum) {
-		this.sum = sum;
-	}
-
-   private void setSum(double[] value_array)
+   private void setSum(Integer value) //Somma un valore
    {
-	   for(double v:value_array)
-	   {
-		   sum+=v;
-	   }
+	   sum+=value;
    }
    
-	public double getMin() {
+	public int getMin() {
 		return min;
 	}
 
 
-	public void setMin(double min) {
+	private void setMin(int min) {
 		this.min = min;
 	}
+	
 
-	private void setMin(double[]value_array)
-	{
-		double min=value_array[0];
-		for (int i=1;i<value_array.length;i++)
-		{
-			if( value_array[i]<min)
-			{
-				min=value_array[i];
-			}
-		}
-		
-		setMin(min);
-	}
-
-	public double getMax() {
+	public int getMax() {
 		return max;
 	}
 
 
-	public void setMax(double max) {
+	private void setMax(int max) {
 		this.max = max;
 	}
 
-	private void setMax(double[]value_array)
-	{
-		double max=value_array[0];
-		for (int i=1;i<value_array.length;i++)
-		{
-			if( value_array[i]>max)
-			{
-				max=value_array[i];
-			}
-		}
-		
-		setMax(max);
-	}
 
 	public double getDev_std() {
+		calculateDev_Std();
 		return dev_std;
 	}
 
 
-	public void setDev_std(double dev_std) {
-		this.dev_std = dev_std;
-	}
-
-	private void setDev_std(double[] value_array) {
-        double sum = 0;
-        for (double v : value_array) {
+	private void calculateDev_Std() {
+		double sum = 0;
+        for (int v : this.stocksData) {
             sum += Math.pow(v - avg, 2);
         }
         
         setDev_std((double) Math.sqrt(sum/count));     
-    }
+	}
+
+
+	private void setDev_std(double dev_std) {
+		this.dev_std = dev_std;
+	}
+
 
 	public int getCount() {
 		return count;
 	}
 
 
-	public void setCount(int count) {
-		this.count = count;
-	}
-	
-	
-	
 
 }
